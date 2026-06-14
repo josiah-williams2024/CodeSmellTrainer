a5146545a110e8a5ae3cb7057e7347917793e88b
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-14 10:54:59 -0400

    Creat relationship

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Deck extends Model
 {
     protected $guarded = [];
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
+    }
 }
