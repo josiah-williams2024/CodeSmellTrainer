7e74df1c3b9eee717d90ba5217a060d8e28d88cc
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-16 11:15:32 -0400

    rename deck route name

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,13 @@ Route::inertia('/', 'Welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
     Route::inertia('game', 'Game')->name('game');
-    Route::get('game/deck1', function () {
+    Route::get('game/deckLongMethod', function () {
         $deck = Deck::query()->with('cards')->findOrFail(1);
 
         return inertia('GameDeck/LongMethod', [
             'deck' => $deck,
         ]);
-    })->name('deck1');
+    })->name('deckLongMethod');
 });
 
 require __DIR__.'/settings.php';
