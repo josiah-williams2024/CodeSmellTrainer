e27fbb66ca0713aeffeb68a5d916b6dd4c99c141
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-18 09:37:12 -0400

    WIP Card deck view

--- resources/js/pages/Game.vue
+++ resources/js/pages/Game.vue
@@ -4,10 +4,39 @@ import { deckLongMethod } from '@/routes';
 </script>
 
 <template>
-    <main class="flex flex-col gap-3 p-4">
+    <main class="minh-screen p-4 md:p-6 lg:p-8">
+        <header class="mb-8">
+            <h1 class="text-center text-3xl font-bold underline">
+                Select which deck to play down below
+            </h1>
+        </header>
+        <section
+            class="mx-auto grid max-w-6xl gap-4 md:grid-cols-2 lg:grid-cols-3"
+        >
+            <article>
+                <Link :href="deckLongMethod()" class="block p-5"
+                    >Long Method Deck</Link
+                >
+            </article>
 
-    <h1 class="flex justify-center">You have arrived Game Page, Select which deck to play</h1>
-    <Link :href="deckLongMethod()" class="underline">Long Method Deck</Link>
+            <article>
+                <Link class="block p-5"
+                    >Clutter Deck</Link
+                >
+            </article>
 
+            <article>
+                <Link class="block p-5"
+                    >Duplication Deck</Link
+                >
+            </article>
+
+        </section>
     </main>
 </template>
+
+<style scoped lang="postcss">
+article {
+    @apply rounded-lg border border-gray-200 bg-white p-5 shadow-sm hover:shadow-md;
+}
+</style>
