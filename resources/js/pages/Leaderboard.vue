9f47817ce668b4bb5eab1a0aaf6373b73978c8d4
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-06-30 09:30:11 -0400

    Create placeholder pages for dashboard features

---
+++ resources/js/pages/Leaderboard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    PlaceHolder for the Leaderboard Page
+</template>
+
+<style scoped></style>
---
+++ resources/js/pages/Stats.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    Place Holder for Stats Page
+</template>
+
+<style scoped></style>
