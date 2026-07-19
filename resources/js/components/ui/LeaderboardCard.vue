5433d2f6eb07c7f14f79a40ff01a928cef7c86ee
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-07-18 20:24:43 -0400

    reformat leaderboard view

--- resources/js/components/ui/LeaderboardCard.vue
+++ resources/js/components/ui/LeaderboardCard.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 type LeaderboardEntry = {
     rank: number;
     name: string;
@@ -10,50 +9,42 @@ defineProps<{
     title: string;
     leaderboard: LeaderboardEntry[];
 }>();
-
 </script>
-    <template>
-        <article
-            class="flex h-64 flex-col rounded-xl border-2 border-primary bg-card p-6 shadow-xl"
-        >
-            <h2 class="mb-3 text-2xl font-semibold">
-                {{ title }}
-            </h2>
 
-            <div class="space-y-3">
-                <template v-if="leaderboard.length > 0">
-                    <div
-                        v-for="player in leaderboard"
-                        :key="player.rank"
-                        class="flex justify-between border-b pb-1"
-                    >
+<template>
+    <article
+        class="flex h-64 flex-col rounded-xl border-2 border-primary bg-card p-6 shadow-xl"
+    >
+        <h2 class="mb-4 text-2xl font-semibold">
+            {{ title }}
+        </h2>
+
+        <div class="flex flex-1 flex-col justify-between">
+            <div
+                v-for="rank in 5"
+                :key="rank"
+                class="flex justify-between border-b pb-2"
+            >
+                <template v-if="leaderboard[rank - 1]">
                     <span class="text-primary">
-                        {{ player.rank }}. {{ player.name }}
+                        {{ rank }}. {{ leaderboard[rank - 1].name }}
                     </span>
 
-                        <span class="font-medium text-card-foreground">
-                        {{ player.averageScore.toFixed(1) }}
+                    <span class="font-medium text-card-foreground">
+                        {{ leaderboard[rank - 1].averageScore.toFixed(1) }}
                     </span>
-                    </div>
                 </template>
 
                 <template v-else>
-                    <div
-                        v-for="rank in 5"
-                        :key="rank"
-                        class="flex justify-between border-b pb-1"
-                    >
                     <span class="text-primary">
                         {{ rank }}. --
                     </span>
 
-                        <span class="font-medium text-card-foreground">
+                    <span class="font-medium text-card-foreground">
                         --
                     </span>
-                    </div>
                 </template>
             </div>
-        </article>
-    </template>
-
-
+        </div>
+    </article>
+</template>
