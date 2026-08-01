e30a3d1be3bbbf61910f7bee24f5caaf9af637a0
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-07-31 20:49:53 -0400

    Stats refactor

--- resources/js/pages/Stats.vue
+++ resources/js/pages/Stats.vue
@@ -2,25 +2,38 @@
 import { Head } from '@inertiajs/vue3';
 import StatsCard from '@/components/ui/StatsCard.vue';
 
-const props = defineProps<{
-    stats: Array<any>;
-}>();
+type Stats = {
+    id: number;
+    name: string;
+    gamesPlayed: number | null;
+    totalPlaytime: number | null;
+    averageAccuracy: number | null;
+    averageScore: number | null;
+    averageTime: number | null;
+};
 
+defineProps<{
+    stats: Stats[];
+}>();
 </script>
 
 <template>
     <Head title="Stats"></Head>
     <main class="min-h-screen bg-background md:p-6 lg:p-8">
         <header class="mb-8">
-            <h1 class="text-center text-3xl font-bold text-foreground">Deck Stats</h1>
+            <h1 class="text-center text-3xl font-bold text-foreground">
+                Deck Stats
+            </h1>
         </header>
 
         <section
             class="mx-auto grid max-w-6xl gap-6 md:grid-cols-2 lg:grid-cols-3"
         >
-            <StatsCard title="Long Method Stats" :stats="props.stats[0]" />
-            <StatsCard title="Clutter Stats" :stats="props.stats[1]" />
-            <StatsCard title="Duplication Stats" :stats="props.stats[2]"/>
+            <StatsCard
+                v-for="stat in stats"
+                :key="stat.id"
+                :stats="stat"
+            />
         </section>
     </main>
 </template>
