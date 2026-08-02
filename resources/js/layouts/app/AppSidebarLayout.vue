9097cae89a6b9bc7a95e137d0bfbc8256f125e75
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-08-02 14:13:43 -0400

    Add trigger so you can see drawer on mobile

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -2,6 +2,7 @@
 import AppContent from '@/components/AppContent.vue';
 import AppShell from '@/components/AppShell.vue';
 import AppSidebar from '@/components/AppSidebar.vue';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/sonner';
 import type { BreadcrumbItem } from '@/types';
 
@@ -18,6 +19,9 @@ withDefaults(defineProps<Props>(), {
     <AppShell variant="sidebar">
         <AppSidebar />
         <AppContent variant="sidebar" class="overflow-x-hidden">
+            <header class="flex h-14 items-center border-b px-4 md:hidden">
+                <SidebarTrigger />
+            </header>
             <slot />
         </AppContent>
         <Toaster />
